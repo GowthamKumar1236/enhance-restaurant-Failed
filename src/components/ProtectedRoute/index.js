7a7584b54395073d4320086b776efd8a68d02d37
Gowtham <gowthamkumar.p1236@gmail.com>
2024-05-20 12:16:52 +0000

    solution

---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import {Redirect, Route} from 'react-router-dom'
+import Cookies from 'js-cookie'
+
+const ProtectedRoute = props => {
+  const jwtToken = Cookies.get('jwt_token')
+
+  if (!jwtToken) {
+    return <Redirect to="/login" />
+  }
+
+  return <Route {...props} />
+}
+
+export default ProtectedRoute
